4b726de84acb05ff245f842b9b60c8d965cc1bb8
João Paulo Sabino <j.paulo_os@hotmail.com>
2021-03-12 19:34:36 -0300

    First Commit

---
+++ ExamplePodd/ExamplePodd.h
@@ -0,0 +1,18 @@
+//
+//  ExamplePodd.h
+//  ExamplePodd
+//
+//  Created by João Paulo Sabino on 12/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExamplePodd.
+FOUNDATION_EXPORT double ExamplePoddVersionNumber;
+
+//! Project version string for ExamplePodd.
+FOUNDATION_EXPORT const unsigned char ExamplePoddVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExamplePodd/PublicHeader.h>
+
+
---
+++ ExamplePodd/MainClass.swift
@@ -0,0 +1,14 @@
+//
+//  MainClass.swift
+//  ExamplePodd
+//
+//  Created by João Paulo Sabino on 12/03/21.
+//
+
+import Foundation
+
+final public class MainClass {
+    static public func testing() {
+        print("ITS WORKING!")
+    }
+}
